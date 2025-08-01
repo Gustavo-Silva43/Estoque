a7b6507372a9cc911767c9bfae09e043eed631b1
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2025-08-01 17:31:04 -0300

    Update admin.py

--- produto/admin.py
+++ produto/admin.py
@@ -15,3 +15,12 @@ class ProdutoAdmin(admin.ModelAdmin):
     )
     search_fields = ('produto',)
     list_filter = ('importado',)
+    actions = ('export_as_csv', 'export_as_xlsx')
+
+    class Media:
+        js = (
+            'https://code.jquery.com/jquery-3.3.1.min.js',
+            '/static/js/estoque_admin.js'
+        )
+
+    def export_as_csv(self, request, queryset):
