a06cdd67308b0bc7b8b22db2aaf9168c3588f81a
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2025-08-03 12:35:04 -0300

    Update urls.py

--- Project/urls.py
+++ Project/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),     
-    path('estoque/', include('estoque.urls')),  
     path('produto/', include('produto.urls')), 
+    path('estoque/', include('estoque.urls')), 
+path('accounts/', include(('django.contrib.auth.urls', 'accounts'), namespace='accounts')),
+ 
 ]
