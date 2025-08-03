13b763e0ef9634db07c0e7dd0ddda5217a5b6c82
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2025-08-03 12:33:22 -0300

    Update models.py

--- produto/models.py
+++ produto/models.py
@@ -1,4 +1,6 @@
+
 from django.db import models
+from django.urls import reverse_lazy
 
 class Produto(models.Model): 
     importado = models.BooleanField(default=False)
@@ -7,9 +9,36 @@ class Produto(models.Model):
     preco = models.DecimalField('preço', max_digits=7, decimal_places=2)
     estoque = models.IntegerField('estoque atual', default=0) 
     estoque_minimo = models.IntegerField('estoque mínimo', default=0)
+    
+    # ADICIONE ESTA LINHA:
+    categoria = models.ForeignKey(
+        'Categoria',  
+        on_delete=models.SET_NULL, 
+        null=True,                 
+        blank=True                 
+    )
 
     class Meta:
         ordering = ('produto',)
 
     def __str__(self):
         return self.produto
+
+    def get_absolute_url(self):
+        return reverse_lazy('produto:produto_detail', kwargs={'pk': self.pk})
+    
+    def to_dict_json(self):
+        return{
+            'pk': self.pk,
+            'produto': self.produto,
+            'estoque': self.estoque,
+       }
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('categoria',)
+
+    def __str__(self):
+        return self.categoria
