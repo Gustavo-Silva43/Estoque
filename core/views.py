ee6c1ea8335340e16273261e6e85ba2df2ed7431
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2025-08-03 12:17:35 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    return render(request, 'core/index.html')
+    return render(request, 'index.html') 
+
+def product_list(request):
+    return render(request, 'core/product_list.html', {}) 
