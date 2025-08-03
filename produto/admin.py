e43559774385684ac2598a89e15996c7e92b236f
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2025-08-03 12:31:59 -0300

    Update admin.py

--- produto/admin.py
+++ produto/admin.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from .models import Produto 
+from .models import Produto, Categoria 
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
@@ -24,3 +24,73 @@ class ProdutoAdmin(admin.ModelAdmin):
         )
 
     def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.field]
+
+        response = HttpResponse(context_type='text/csv')
+        respnse[
+            'Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer = writerow(field_names)
+        for obj in queryset:
+            row = write.writerow([getattr(obj,field)
+                                        for field in field_names])
+        return response
+
+    export_as_csv.short_description = "Exportat CSV"
+
+    def export_as_xlsx(self,request,queryset):
+
+        meta = self.model._meta
+        columns = (
+            'Importado',
+            'NCM',
+            'Produto',
+            'Preço',
+            'Estoque',
+            'Estoque mínimo',
+            'Categoria'
+        )
+
+        response = HttpResponse(content_type='application/ms-excel')
+        response[
+            'Content-Disposition'] = 'attachment; filename="%s_%s.xlsx"' % (meta,MDTA)
+
+        wb = xlwt.Workbook(encoding='utf-8')
+        ws = wb.add_sheet(self.model.__name__)
+
+        row_num = 0
+
+        font_style = xlwt.XFStyle()
+        font_style.font.bold = True 
+
+        for col_num in range(len(columns)):
+            ws.write(row_num, col_num, columns[col_num], font_style)
+        
+        default_style = xlwt.XFStyle()
+
+        rows = queryset.values_list(
+            'importado',
+            'ncm',
+            'produto',
+            'preco',
+            'estoque',
+            'estoque_minimo',
+            'categoria__categoria'
+        )
+        for row, rowdata in enumerate(rows):
+            row_num += 1
+            for col, val in enumerante(rowdata):
+                ws.write(row_num, col, val, default_style)
+
+        wb.save(response)
+        return response
+
+    export_as_xlsx.short_description = "Exportar XLSX"
+
+    @admin.register(Categoria)
+    class CategoriaAdmin(admin.ModelAdmin):
+        list_display = ('__str__', )
+        search_fields = ('categoria',)
