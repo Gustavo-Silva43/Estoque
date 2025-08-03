821cafd98a092f9706f70c4c9d3e5608e6a9b83b
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2025-08-03 12:17:08 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'core' # Isso registra o namespace 'core' para ser usado com reverse()
+app_name = 'core'
 
 urlpatterns = [
-    # Mapeia a URL raiz de 'core/' para a view 'index'
     path('', views.index, name='index'),
-
-    # Mapeia 'core/products/' para a view 'product_list'
-    # Ajuste o nome da URL 'products/' se quiser algo diferente
     path('products/', views.product_list, name='product_list'),
 ]
