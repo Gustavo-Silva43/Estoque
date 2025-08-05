cfb46432b1a06c91001b29dddf4ecccb636dc133
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2025-08-05 17:39:05 -0300

    Update views.py

--- estoque/views.py
+++ estoque/views.py
@@ -1,12 +1,13 @@
-
-from multiprocessing import context
-from pyexpat.errors import messages
 from django.forms import inlineformset_factory
-from django.shortcuts import render, redirect, resolve_url 
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect 
+from django.shortcuts import render, redirect, resolve_url
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
+from django.contrib import messages
+
+# Certifique-se de importar o seu modelo 'Produto'
+from produto.models import Produto
 
 from .forms import EstoqueForm, EstoqueItensEntradaForm, EstoqueItensSaidaForm
 from .models import (
@@ -17,26 +18,32 @@ from .models import (
     ProtocoloEntrega
 )
 
+
+# View de listagem para entrada
 def estoque_entrada_list(request):
     template_name = 'estoque_list.html'
     objects = EstoqueEntrada.objects.all()
     context = {
         'object_list': objects,
-        'title': 'Entrada',
-        'url_add':'estoque:estoque_entrada_add',
+        'titulo': 'Entrada',
+        'url_add': 'estoque:estoque_entrada_add',
     }
-    return render()(request, template_name, context)
+    return render(request, template_name, context)
 
+
+# View de listagem para entrada (baseada em classe)
 class EstoqueEntradaList(ListView):
     model = EstoqueEntrada
     template_name = 'estoque_list.html'
 
     def get_context_data(self, **kwargs):
         context = super(EstoqueEntradaList, self).get_context_data(**kwargs)
-        context['title'] = 'Entrada'
+        context['titulo'] = 'Entrada'
         context['url_add'] = 'estoque:estoque_entrada_add'
         return context
 
+
+# View de detalhes para entrada
 def estoque_entrada_detail(request, pk):
     template_name = 'estoque_detail.html'
     obj = EstoqueEntrada.objects.get(pk=pk)
@@ -46,19 +53,25 @@ def estoque_entrada_detail(request, pk):
     }
     return render(request, template_name, context)
 
+
+# View de detalhes (baseada em classe)
 class EstoqueDetail(DetailView):
     model = Estoque
     template_name = 'estoque_detail.html'
 
+
+# Função para dar baixa no estoque
 def dar_baixa_estoque(form):
-    produtos = form.estoque.all()
+    produtos = form.estoqueitens_set.all()
     for item in produtos:
-        produto = produto.objects.get(pk=item.produto.pk)
+        produto = Produto.objects.get(pk=item.produto.pk)
         produto.estoque = item.saldo
-        produto.save()   
+        produto.save()
     print('Estoque atualizado com sucesso!')
 
-def estoque_add(request, form_inline, template_name,movimento, url):
+
+# Função genérica para adicionar estoque (entrada/saída)
+def estoque_add(request, form_inline, template_name, movimento, url):
     estoque_form = Estoque()
     item_estoque_formset = inlineformset_factory(
         Estoque,
@@ -67,7 +80,7 @@ def estoque_add(request, form_inline, template_name,movimento, url):
         extra=0,
         can_delete=True,
         min_num=1,
-    )    
+    )
     if request.method == 'POST':
         form = EstoqueForm(request.POST, instance=estoque_form, prefix='main')
         formset = item_estoque_formset(
@@ -77,7 +90,7 @@ def estoque_add(request, form_inline, template_name,movimento, url):
         )
         if form.is_valid() and formset.is_valid():
             form = form.save(commit=False)
-            form.fucnionario = request.user
+            form.funcionario = request.user
             form.movimento = movimento
             form.save()
             formset.save()
@@ -86,11 +99,14 @@ def estoque_add(request, form_inline, template_name,movimento, url):
     else:
         form = EstoqueForm(instance=estoque_form, prefix='main')
         formset = item_estoque_formset(instance=estoque_form, prefix='estoque')
-   
-    context = {'form': form, 'formset': formset,}
+
+    context = {'form': form, 'formset': formset, }
     return context
 
+
+# View para adicionar entrada de estoque
 @login_required
+@permission_required('estoque.pode_adicionar_estoque', raise_exception=True)
 def estoque_entrada_add(request):
     form_inline = EstoqueItensEntradaForm
     template_name = 'estoque_entrada_form.html'
@@ -101,6 +117,8 @@ def estoque_entrada_add(request):
         return redirect(resolve_url(url, pk=response.get('pk')))
     return render(request, template_name, response)
 
+
+# View de listagem para saída
 def estoque_saida_list(request):
     template_name = 'estoque_list.html'
     objects = EstoqueSaida.objects.all()
@@ -111,6 +129,8 @@ def estoque_saida_list(request):
     }
     return render(request, template_name, context)
 
+
+# View de listagem para saída (baseada em classe)
 class EstoqueSaidaList(ListView):
     model = EstoqueSaida
     template_name = 'estoque_list.html'
@@ -120,7 +140,9 @@ class EstoqueSaidaList(ListView):
         context['titulo'] = 'Saída'
         context['url_add'] = 'estoque:estoque_saida_add'
         return context
-    
+
+
+# View de detalhes para saída
 def estoque_saida_detail(request, pk):
     template_name = 'estoque_detail.html'
     obj = EstoqueSaida.objects.get(pk=pk)
@@ -130,7 +152,10 @@ def estoque_saida_detail(request, pk):
     }
     return render(request, template_name, context)
 
+
+# View para adicionar saída de estoque
 @login_required
+@permission_required('estoque.pode_adicionar_estoque', raise_exception=True)
 def estoque_saida_add(request):
     form_inline = EstoqueItensSaidaForm
     template_name = 'eestoque_saida_form.html'
@@ -141,6 +166,8 @@ def estoque_saida_add(request):
         return HttpResponseRedirect(resolve_url(url, context.get('pk')))
     return render(request, template_name, context)
 
+
+# View para listagem de protocolos de entrega
 def protocolo_de_entrega(request):
     template_name = 'protocolo_de_entrega_list.html'
     objects = ProtocoloEntrega.objects.all()
@@ -149,9 +176,11 @@ def protocolo_de_entrega(request):
     }
     return render(request, template_name, context)
 
+
+# View de detalhes para protocolos de entrega
 @login_required
 def protocolo_de_entrega_detail(request, pk):
-    template_name = 'protocolo_de_entrega_detail.html' 
+    template_name = 'protocolo_de_entrega_detail.html'
     obj = ProtocoloEntrega.objects.get(pk=pk)
     context = {
         'object': obj,
@@ -159,12 +188,15 @@ def protocolo_de_entrega_detail(request, pk):
     return render(request, template_name, context)
 
 
+# View para dar baixa no estoque com protocolo de entrega
+@login_required
+@permission_required('estoque.pode_excluir_estoque', raise_exception=True)
 def dar_baixa_no_estoque_com_protocolo_de_entrega(request, pk):
     entrega = ProtocoloEntrega.objects.get(pk=pk)
 
     if entrega.estoque_atualizado:
         messages.error(request, 'Já foi dado baixa nessa entrega.')
-        return HttpResponseRedirect(reverse('estoque:produto_list')) 
+        return HttpResponseRedirect(reverse('estoque:produto_list'))
 
     estoque_saida = EstoqueSaida.objects.create(
         funcionario=request.user,
@@ -175,16 +207,16 @@ def dar_baixa_no_estoque_com_protocolo_de_entrega(request, pk):
             estoque=estoque_saida,
             produto=item.produto,
             quantidade=item.quantidade,
-            saldo=item.produto.estoque - item.quantidade, 
+            saldo=item.produto.estoque - item.quantidade,
         )
-    
-    produtos_para_atualizar = estoque_saida.estoqueitens_set.all() 
+
+    produtos_para_atualizar = estoque_saida.estoqueitens_set.all()
     for item_produto in produtos_para_atualizar:
-        produto = item_produto.produto 
-        produto.estoque = item_produto.saldo 
+        produto = item_produto.produto
+        produto.estoque = item_produto.saldo
         produto.save()
-    
+
     entrega.estoque_atualizado = True
     entrega.save()
     messages.success(request, 'Estoque atualizado com sucesso!')
-    return HttpResponseRedirect(reverse('produto:produto_list')) 
+    return HttpResponseRedirect(reverse('produto:produto_list'))
